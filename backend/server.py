a43e47b0a6e2885868254137784da72d15a81293
Gregor Sturm <mail@gregor-sturm.de>
2018-09-15 16:17:29 +0200

    indent

--- backend/server.py
+++ backend/server.py
@@ -18,10 +18,12 @@ productList = []
 with open('buhlerFoodprint.json') as f:
     productList = json.load(f)
 
+
 @app.route('/')
 def basic():
     # show the post with the given id, the id is an integer
     return 'Server is running... If you post something and are logged in to the API, you\'d get a more useful thing'
+
 
 @auth.get_password
 def get_password(username):
@@ -29,92 +31,97 @@ def get_password(username):
         return ''
     return None
 
+
 @app.route('/start/<userName>', methods=['POST'])
 def startChallenge(userName):
-		newUser = {}
-		newUser['name'] = username
-		newUser['points'] = totalSum
-		newUser['badges'] = []
-		userId = len(userList)
-		newUser['id'] = userId
-		userList[username] = newUser
-		return json.dumps(newUser)
+    newUser = {}
+    newUser['name'] = username
+    newUser['points'] = totalSum
+    newUser['badges'] = []
+    userId = len(userList)
+    newUser['id'] = userId
+    userList[username] = newUser
+    return json.dumps(newUser)
+
 
 @app.route('/getProductList/', methods=['GET'])
 def getProductList():
-	return json.dumps(productList)
+    return json.dumps(productList)
+
 
 @app.route('/boughtProducts/<username>', methods=['POST'])
 def calculateSum(username):
-	content = request.json
-	totalSum = 0
-	boughtItems = []
+    content = request.json
+    totalSum = 0
+    boughtItems = []
 
-	for i in range(0, len(content)):
-		ident = content[i]
-		product = productList[ident]
-		pts = product['Points']
-		qty = randint(1,20)
-		itemPts = pts*qty
-		newItem = {}
-		newItem['Name'] = product['Name']
-		newItem['Points'] = product['Points']
-		newItem['Category'] = product['Category']
-		newItem['Quantity'] = qty
-		newItem['ItemPoints'] = itemPts		
-		#print("we had " + product['Name'] + " which had " + str(pts) + " points, and we bought " + str(qty) + " of them, which gives us a total of " + str(itemPts))
-		totalSum = totalSum + itemPts
-		boughtItems.append(newItem)
+    for i in range(0, len(content)):
+        ident = content[i]
+        product = productList[ident]
+        pts = product['Points']
+        qty = randint(1, 20)
+        itemPts = pts * qty
+        newItem = {}
+        newItem['Name'] = product['Name']
+        newItem['Points'] = product['Points']
+        newItem['Category'] = product['Category']
+        newItem['Quantity'] = qty
+        newItem['ItemPoints'] = itemPts
+        # print("we had " + product['Name'] + " which had " + str(pts) + " points, and we bought " + str(qty) + " of them, which gives us a total of " + str(itemPts))
+        totalSum = totalSum + itemPts
+        boughtItems.append(newItem)
 
-	if not username in userList:
-		newUser = {}
-		newUser['name'] = username
-		newUser['points'] = totalSum
-		newUser['badges'] = []
-		userId = len(userList)
-		newUser['id'] = userId
-		userList[username] = newUser
-	else:
-		oldUser = userList[username]
-		oldUser['points'] = totalSum
+    if not username in userList:
+        newUser = {}
+        newUser['name'] = username
+        newUser['points'] = totalSum
+        newUser['badges'] = []
+        userId = len(userList)
+        newUser['id'] = userId
+        userList[username] = newUser
+    else:
+        oldUser = userList[username]
+        oldUser['points'] = totalSum
 
-	result = {} 
-	result['BoughtItems'] = boughtItems
-	result['Sum'] = totalSum
-	return json.dumps(result)
+    result = {}
+    result['BoughtItems'] = boughtItems
+    result['Sum'] = totalSum
+    return json.dumps(result)
+
 
 @app.route('/getChallengeState/<username>', methods=['POST'])
 def getChallengeState(username):
-	content = request.json
-	thisUsername = content['Me'] 
-	otherUsername = content['Adversary']
-	thisPoints = 0
-	otherPoints = 0
-	resultStr = ""
+    content = request.json
+    thisUsername = content['Me']
+    otherUsername = content['Adversary']
+    thisPoints = 0
+    otherPoints = 0
+    resultStr = ""
 
-	if thisUsername in userList:
-		thisUser = userList[thisUsername]
-		thisPoints = thisUser['points']
-	else:
-		print("ERROR! You never saved your own points!!")
-		return make_response(jsonify({'Error!': 'Bad request format! Did you save your own points before quering it?'}), 400)
+    if thisUsername in userList:
+        thisUser = userList[thisUsername]
+        thisPoints = thisUser['points']
+    else:
+        print("ERROR! You never saved your own points!!")
+        return make_response(jsonify({'Error!': 'Bad request format! Did you save your own points before quering it?'}),
+                             400)
 
-	if not otherUsername in userList:
-		resultStr = "Ongoing"
-	else:
-		otherUser = userList[otherUsername]
-		otherPoints = otherUser['points']
+    if not otherUsername in userList:
+        resultStr = "Ongoing"
+    else:
+        otherUser = userList[otherUsername]
+        otherPoints = otherUser['points']
 
-		if thisPoints > otherPoints:
-			resultStr = "Winner"
-		elif otherPoints > thisPoints:
-			resultStr = "Looser"
-		else:
-			resultStr = "Tie!"
+        if thisPoints > otherPoints:
+            resultStr = "Winner"
+        elif otherPoints > thisPoints:
+            resultStr = "Looser"
+        else:
+            resultStr = "Tie!"
 
-	result = {}
-	result['ChallengeResult'] = resultStr
-	return json.dumps(result)
+    result = {}
+    result['ChallengeResult'] = resultStr
+    return json.dumps(result)
 
-#start stop
-#get shoppin
+# start stop
+# get shoppin
