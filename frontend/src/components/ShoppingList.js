7f3acb282d438291b0c7ac63fba6908abdb778c3
Sebastian Wilzbach <seb@wilzba.ch>
2018-09-15 02:29:15 +0200

    Fix mobx

--- frontend/src/components/ShoppingList.js
+++ frontend/src/components/ShoppingList.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
 
+import store from '../store'
+
+@observer
 class ShoppingList extends Component {
   render() {
     return (
--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { observer } from 'mobx-react';
 
+import store from '../store'
+
+@observer
 class UserProfile extends Component {
   render() {
     return (
       <div className="UserProfile">
         <Link to='/shopping-list'>Start shopping</Link>
+        <div>Username: {store.username}</div>
       </div>
     );
   }
