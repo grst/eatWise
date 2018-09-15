29db21dc5574abfa51665f83a6dae57b8e583398
Sebastian Wilzbach <seb@wilzba.ch>
2018-09-15 02:23:54 +0200

    start shopping

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class UserProfile extends Component {
   render() {
     return (
       <div className="UserProfile">
-        user profile
+        <Link to='/shopping-list'>Start shopping</Link>
       </div>
     );
   }
