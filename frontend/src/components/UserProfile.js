c2b0c48557cc3b7ea38289028f194372a91ab879
Gregor Sturm <mail@gregor-sturm.de>
2018-09-15 02:51:14 +0200

    add avatar

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const styles = {
+  row: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+};
 
 class UserProfile extends Component {
   render() {
     return (
       <div className="UserProfile">
+        <div className={styles.row}>
+          <Avatar
+            alt="User1"
+            src="/img/user1.png"
+            className={styles.bigAvatar}
+          />
+        </div>
         <Link to='/shopping-list'>Start shopping</Link>
       </div>
     );
