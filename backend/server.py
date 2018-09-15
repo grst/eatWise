3e9bbddcd85d89327ab8ae7d17c4c6afcd8a8aa5
Mariana Avezum <m.avezum@tum.de>
2018-09-15 16:36:31 +0200

    updated post username

--- backend/server.py
+++ backend/server.py
@@ -23,17 +23,14 @@ def basic():
     # show the post with the given id, the id is an integer
     return 'Server is running... If you post something and are logged in to the API, you\'d get a more useful thing'
 
-@auth.get_password
-def get_password(username):
-    if username == 'jabberwocky':
-        return ''
-    return None
-
-@app.route('/start/<userName>', methods=['POST'])
-def startChallenge(userName):
+@app.route('/start/', methods=['POST'])
+def startChallenge():
+		content = request.json
+		username = content['username']
 		newUser = {}
 		newUser['name'] = username
 		newUser['points'] = 0
+		newUser['co2'] = 0
 		newUser['badges'] = []
 		userId = len(userList)
 		newUser['id'] = userId
@@ -44,14 +41,16 @@ def startChallenge(userName):
 def getProductList():
 	return json.dumps(productList)
 
-@app.route('/buyProducts/<username>', methods=['POST'])
-def calculateSum(username):
+@app.route('/buyProducts/', methods=['POST'])
+def calculateSum():
 	seed(42)
 	content = request.json
 	boughtItems = []
+	username = content['username']
+	products = content['products']
 
-	for i in range(0, len(content)):
-		ident = content[i]
+	for i in range(0, len(products)):
+		ident = products[i]
 		product = productList[ident]
 		qty = randint(100,3000)
 		newItem = {}
@@ -84,8 +83,8 @@ def calculateSum(username):
 	result['BasketPoints'] = basketPoints
 	return json.dumps(result)
 
-@app.route('/getChallengeState/<username>', methods=['POST'])
-def getChallengeState(username):
+@app.route('/getChallengeState/', methods=['POST'])
+def getChallengeState():
 	content = request.json
 	thisUsername = content['Me'] 
 	otherUsername = content['Adversary']
@@ -100,7 +99,7 @@ def getChallengeState(username):
 		print("ERROR! You never saved your own points!!")
 		return make_response(jsonify({'Error!': 'Bad request format! Did you save your own points before quering it?'}), 400)
 
-	if not otherUsername in userList:
+	if (not otherUsername in userList):
 		resultStr = "Ongoing"
 	else:
 		otherUser = userList[otherUsername]
