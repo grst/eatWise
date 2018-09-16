7980b93d975c7b62aa8e8260056e31382447a32f
Sebastian Wilzbach <seb@wilzba.ch>
2018-09-16 03:45:13 +0200

    Rename supermarket title

--- frontend/src/components/SupermarketConfirmation.js
+++ frontend/src/components/SupermarketConfirmation.js
@@ -32,7 +32,7 @@ class SupermarketConfirmation extends React.Component {
   }
 
   componentDidMount() {
-    store.pageTitle = "Waiting for supermarket";
+    store.pageTitle = "Processing...";
     const query = qs.parse(this.props.location.search);
     // just check if set for the sake of simplicity
     if (!query.skipWaiting) {
